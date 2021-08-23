d2e115cf9f54339ccc59193291e3f1e9776687e1
alikdexor <alikdexor@gmail.com>
2021-08-23 12:46:42 +0300

    Add page cache, request params filter

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,11 +1,18 @@
 <?php
+
 namespace Adx\PagesModule\Controller;
 
 use Adx\PagesModule\Model\Page;
+use App\Cache\PageCache;
+use RS\Component\Core\Filter\QueryParamsFilter;
+use RS\Component\Core\Settings\SettingsInterface;
 use Yii;
+use yii\base\InvalidConfigException;
 use yii\base\ViewContextInterface;
+use yii\di\NotInstantiableException;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
+use yii\web\Request;
 use yii\web\Response;
 
 /**
@@ -13,13 +20,40 @@ use yii\web\Response;
  */
 class MainController extends Controller implements ViewContextInterface
 {
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors(): array
+    {
+        return [
+            'queryParams' => [
+                'class' => QueryParamsFilter::class,
+                'actions' => [
+                    'index' => ['id', 'slug'],
+                ],
+            ],
+            'pageCache' => [
+                'class' => PageCache::class,
+                'enabled' => (bool) $this->get(SettingsInterface::class)->get('enable_page_cache', false),
+                'duration' => 600,
+                'variations' => [
+                    Yii::$app->language,
+                    $this->action->id,
+                    \implode(':', \array_values($this->get(Request::class)->get())),
+                    $this->isMobile(),
+                ],
+            ],
+        ];
+    }
+
     /**
      * Переопределяет дефолтный путь шаблонов модуля.
      * Путь задается в конфиге модуля, в компонентах приложения.
      *
      * @return string
      */
-    public function getViewPath()
+    public function getViewPath(): string
     {
         return $this->module->getViewPath();
     }
@@ -27,9 +61,14 @@ class MainController extends Controller implements ViewContextInterface
     /**
      * View one custom page.
      *
-     * @return mixed
+     * @param int $id
+     * @param string $slug
+     * @return string
+     * @throws InvalidConfigException
+     * @throws NotFoundHttpException
+     * @throws NotInstantiableException
      */
-    public function actionIndex($id = 0, $slug = '')
+    public function actionIndex(int $id = 0, string $slug = ''): string
     {
         $identify = (0 !== (int) $id) ? (int) $id : $slug;
         $page = $this->findByIdOrSlug($identify);
@@ -47,9 +86,7 @@ class MainController extends Controller implements ViewContextInterface
      * Find page by primary key or by slug
      *
      * @param int|string $identify
-     *
-     * @return Page
-     *
+     * @return array
      * @throws NotFoundHttpException
      */
     public function findByIdOrSlug($identify): array
@@ -79,11 +116,12 @@ class MainController extends Controller implements ViewContextInterface
      * или запрета перехода по ссылкам страницы
      *
      * @param array $page
-     * @return void
+     * @throws InvalidConfigException
+     * @throws NotInstantiableException
      */
     protected function registerXRobotsTag(array $page)
     {
-        $response = Yii::$container->get(Response::class);
+        $response = $this->get(Response::class);
 
         $crawlerRestrictionTypes = [];
         if (true === (bool) $page['noindex']) {
@@ -98,5 +136,32 @@ class MainController extends Controller implements ViewContextInterface
             $headers = $response->getHeaders();
             $headers->add('X-Robots-Tag', \implode(',', $crawlerRestrictionTypes));
         }
+    }
+
+    /**
+     * Get instance by tag name form DI container
+     *
+     * @param string $name
+     * @return object
+     * @throws InvalidConfigException
+     * @throws NotInstantiableException
+     */
+    protected function get(string $name): object
+    {
+        return Yii::$container->get($name);
+    }
+
+    /**
+     * Detect user is mobile device
+     *
+     * @return bool
+     * @throws InvalidConfigException
+     * @throws NotInstantiableException
+     */
+    protected function isMobile(): bool
+    {
+        $deviceDetect = $this->get('device.detect');
+
+        return $deviceDetect->isMobile();
     }
 }
